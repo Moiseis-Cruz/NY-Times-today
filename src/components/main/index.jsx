00de773bc7f6be529fcdf1585082c7e2ec4a8bf0
Moiseis Cruz <moizzito@gmail.com>
2024-01-30 21:23:14 -0300

    I consumed the API.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,3 +1,12 @@
+async function getDatos(){
+    const response = await fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1");
+    const data = await response.json()
+    console.log(data.results);
+    return data.results
+}
+
+getDatos()
+
 export const Main = () => {
     return(
         <section>
