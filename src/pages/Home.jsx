d55362263c6d50a874ccf603cc1571893a1832db
Moiseis Cruz <moizzito@gmail.com>
2024-02-29 19:04:28 -0300

    I created the Home.jsx file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Main } from "../components/main";
+
+export const Home = () => {
+    return(
+        <div>
+            <h1>Página Home</h1>
+            <Main />
+        </div>
+    )
+}
