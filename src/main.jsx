b13d93dfedfea849a12cca63af2902a612aa232d
Moiseis Cruz <moizzito@gmail.com>
2024-02-29 18:59:28 -0300

    Working with RouterProvider and createBrowserRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import { Home } from './pages/Home.jsx';
+import { PostNews } from './pages/PostNews.jsx';
+import { ErrorPage } from './pages/ErrorPage.jsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "news",
+        element: <PostNews />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
