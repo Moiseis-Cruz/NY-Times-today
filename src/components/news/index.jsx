b87b3b72f271738e7f86cff387d81bbfdfa69a53
Moiseis Cruz <moizzito@gmail.com>
2024-02-29 19:12:39 -0300

    I changed the News.jsx file

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-async function getDatos(section){
-    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1/${section}`);
+async function getDatos(){
+    const response = await fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1");
     const data = await response.json()
-    return data
+    return data.results
 }
 
 export const News = () => {
 
-    const { section } = useParams()
-
-    const [ news, setNews ] = useState()
+    const [ news, setNews ] = useState({
+        newsList: []
+    })
 
     useEffect( () => {
         const fetchData = async () => {
-            const data = await getDatos(section);
+            const data = await getDatos();
 
-            setNews(data)
+            setNews({
+                newsList: data
+            })
         }
 
         fetchData()
@@ -25,10 +27,22 @@ export const News = () => {
 
     return(
         <section>
-            <Link to={"/"}>Voltar ao início!</Link>
-            <div>
-                <h2>{news.title}</h2>
-            </div>
+            <ul>
+                {
+                    news.newsList.map((item, index) => {
+                        return(
+                            <li key={index} style={{border: "2px solid green"}}>
+                                <Link to="/news">
+                                    <div>
+                                        <h2>{item.title}</h2>
+                                        {news.newsList === null || news.newsList === undefined ? <img src={item.multimedia[1].url} /> : <p>Imagem não disponível</p>}
+                                    </div>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </section>
     )
 }
