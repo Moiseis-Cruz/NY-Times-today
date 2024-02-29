31dbbe46db17d03ca259b658fe0a980a8c8af4ce
Moiseis Cruz <moizzito@gmail.com>
2024-02-29 19:00:39 -0300

    I changed the "to" of <Link/>

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 async function getDatos(){
     const response = await fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1");
     const data = await response.json()
-    console.log(data);
     return data.results
 }
 
@@ -33,7 +32,7 @@ export const Main = () => {
                     news.newsList.map((item, index) => {
                         return(
                             <li key={index} style={{border: "2px solid green"}}>
-                                <Link to={`/news/${item.section}`}>
+                                <Link to="/news">
                                     <div>
                                         <h2>{item.title}</h2>
                                         {news.newsList === null || news.newsList === undefined ? <img src={item.multimedia[1].url} /> : <p>Imagem não disponível</p>}
