96b34e7b9c69a0e6c1991d4dc69c30f91f82f47b
Moiseis Cruz <moizzito@gmail.com>
2024-03-20 21:39:37 -0300

    I restructured the code to access news individually.

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-async function getDatos(){
-    const response = await fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1");
-    const data = await response.json()
-    return data.results
+async function getDatos(id){
+    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/home/${id}.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1`);
+    const data = await response.json();
+    console.log(data);
+    return data
 }
 
 export const News = () => {
 
-    const [ news, setNews ] = useState({
-        newsList: []
-    })
+    const { id } = useParams()
+
+    const [ news, setNews ] = useState('')
 
     useEffect( () => {
         const fetchData = async () => {
-            const data = await getDatos();
+            const data = await getDatos(id);
+            console.log(data);
 
-            setNews({
-                newsList: data
-            })
+            setNews(data)
         }
 
         fetchData()
@@ -27,22 +27,10 @@ export const News = () => {
 
     return(
         <section>
-            <ul>
-                {
-                    news.newsList.map((item, index) => {
-                        return(
-                            <li key={index} style={{border: "2px solid green"}}>
-                                <Link to="/news">
-                                    <div>
-                                        <h2>{item.title}</h2>
-                                        {news.newsList === null || news.newsList === undefined ? <img src={item.multimedia[1].url} /> : <p>Imagem não disponível</p>}
-                                    </div>
-                                </Link>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <Link to="/">Home</Link>
+            <div>
+                <h2>{news.title}</h2>
+            </div>
         </section>
     )
 }
