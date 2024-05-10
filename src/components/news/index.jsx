27b1b37593e4dcad62478ee1b166a34e9991211a
Moiseis Cruz <moizzito@gmail.com>
2024-05-09 21:17:02 -0300

    I added semicolons to organize it better.

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -2,17 +2,17 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 async function getDatos(id){
-    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/home/${id}.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1`);
+    const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/home/news/${id}.json?api-key=0wYvx3BOlpmF8IVe2RIV0DK1IZv7vXY1`);
     const data = await response.json();
     console.log(data);
-    return data
-}
+    return data;
+};
 
 export const News = () => {
 
-    const { id } = useParams()
+    const { id } = useParams();
 
-    const [ news, setNews ] = useState('')
+    const [ news, setNews ] = useState('');
 
     useEffect( () => {
         const fetchData = async () => {
@@ -20,10 +20,10 @@ export const News = () => {
             console.log(data);
 
             setNews(data)
-        }
+        };
 
         fetchData()
-    }, [])
+    }, []);
 
     return(
         <section>
@@ -33,4 +33,4 @@ export const News = () => {
             </div>
         </section>
     )
-}
+};
